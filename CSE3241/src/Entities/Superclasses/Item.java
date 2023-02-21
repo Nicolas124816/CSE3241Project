33e7b349dd88690377b6a3e11b27d0035361c8f5
Alex Book <book.45@osu.edu>
2023-02-21 18:41:21 -0500

    create fields and getters/setters for entity superclasses

--- CSE3241/src/Entities/Superclasses/Item.java
+++ CSE3241/src/Entities/Superclasses/Item.java
@@ -1,5 +1,60 @@
-package Entities;
+package Entities.Superclasses;
+
+import java.util.Date;
+import java.util.UUID;
 
 public class Item {
+    private UUID inventoryID;
+    private Date expDate;
+    private String description;
+    private String manufacturer;
+    private int modelNumber;
+    private int year;
+    private int serialNumber;
     
+    public UUID getInventoryID() {
+        return inventoryID;
+    }
+    public void setInventoryID(UUID inventoryID) {
+        this.inventoryID = inventoryID;
+    }
+    public Date getExpDate() {
+        return expDate;
+    }
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public int getModelNumber() {
+        return modelNumber;
+    }
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    
+
 }
--- CSE3241/src/Entities/Superclasses/Person.java
+++ CSE3241/src/Entities/Superclasses/Person.java
@@ -1,5 +1,42 @@
 package Entities.Superclasses;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phone;
+    private String email;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getPhone() {
+        return phone;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     
 }
