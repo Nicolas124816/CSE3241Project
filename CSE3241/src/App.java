2d2b5b101253a0d286485e2dd122eac3f682fe0c
Alex Book <book.45@osu.edu>
2023-02-21 19:19:14 -0500

    simple console menu interface

--- CSE3241/src/App.java
+++ CSE3241/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Select one of the options (1, 2, or 3): \n1.) Add New Records \n2.) Edit Existing Records \n3.) Search Existing Records \n");
+        int option = scan.nextInt();
+
+        printEntityOptions();
     }
+
+    private static void printEntityOptions() {
+        System.out.println("Select one of the options (1, 2, or 3): \n1.) Members \n2.) Warehouses \n3.) Equipment");
+    }
+
+    
 }
