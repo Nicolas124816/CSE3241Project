6e37abfa7d1998f16d325c4b28741d1d499484e0
Alex Book <book.45@osu.edu>
2023-03-28 21:35:52 -0400

    3/28 end of day status

--- CSE3241/src/Util/AddRecordUtil.java
+++ CSE3241/src/Util/AddRecordUtil.java
@@ -1,9 +1,42 @@
 package Util;
 
+import java.util.Scanner;
+
+import Input.InputHelper;
+
 public class AddRecordUtil {
     
     public static void addRecord() {
+        Scanner scan = new Scanner(System.in);
+        int entity = InputHelper.getEntityOption(scan);
+        switch(entity) {
+            case 1:
+                RecordUtil.addMember(memberList, scan);
+                continue;
+            case 2:
+                RecordUtil.addWarehouse(warehouseList, scan);
+                continue;
+            case 3:
+                RecordUtil.addEquipment(equipmentList, scan);
+                continue;
+            default:
+                System.out.println("You entered an incorrect entity option");
+                continue;
+        }
+    }
+
+    public static void deleteRecord() {
 
     }
+
+    public static void updateRecord() {
+
+    }
+
+    public static void searchRecord() {
+
+    }
+
+
 
 }
