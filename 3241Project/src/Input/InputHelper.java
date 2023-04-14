3ca73b1a888bc11362a169a26988fd48dd978861
Alex Book <book.45@osu.edu>
2023-04-13 22:06:15 -0400

    add more extra queries

--- 3241Project/src/Input/InputHelper.java
+++ 3241Project/src/Input/InputHelper.java
@@ -18,11 +18,8 @@ public class InputHelper {
         System.out.println("Select one of the options : \n" + "1.) Members \n"
                 + "2.) Warehouses \n" + "3.) Equipment\n" + "4.) Drones \n"
                 + "5.) Reviews \n" + "6.) Employees\n"
-                + "7.) Shipping Containers \n"
-                + "8.) Container Info\n"
-                + "9.) Warehouse order\n"
-                + "10.) Workplace\n"
-                + "11.) Rents\n"
+                + "7.) Shipping Containers \n" + "8.) Container Info\n"
+                + "9.) Warehouse order\n" + "10.) Workplace\n" + "11.) Rents\n"
                 + "12.) Shipment");
         int option = scan.nextInt();
         scan.nextLine();
@@ -33,14 +30,8 @@ public class InputHelper {
         System.out.println("Choose from the following queries: \n"
                 + "1.) Find the description of all equipment by MANUFACTURER released before YEAR\n"
                 + "2.) Give all the watering equipment and their date of their checkout (renting) from a single member\n"
-                + "3.) List all the gardening equipment and their unique identifiers with less than 2 units held by the warehouse.\n"
-                + "4.) Give all the members who checked out (rented) an electric equipment delivered by DRONE and the electric equipment they checked out.\n"
-                + "5.) Find the total number of items rented by a single member\n"
-                + "6.) Find the member who has rented the most computer & internet items and the total number of items they have checked out.\n"
-                + "7.) Find the serial number and type of equipment with 5 star ratings\n"
-                + "8.) Find the serial number and type of equipment that was purchased by members who have been members for 5 or\n"
-                + "more years\n"
-                + "9.) Find all material of shipping containers that have carried equipment over 5 lbs");
+                + "3.) Give number of employees who are also members of the delivery sevice\n"
+                + "4.) Give the average rating for each equipment\n");
         int option = scan.nextInt();
         scan.nextLine();
         return option;
--- 3241Project/src/Util/ExtraQueries.java
+++ 3241Project/src/Util/ExtraQueries.java
@@ -19,12 +19,20 @@ public class ExtraQueries {
             case 2:
                 extraQuery2(scan);
                 break;
+            case 3:
+                extraQuery3(scan);
+                break;
+            case 4:
+                extraQuery4(scan);
+                break;
             default:
                 System.out.println("You didn't enter a valid option");
         }
     }
 
     private static void extraQuery1(Scanner scan) {
+        System.out.println(
+                "Query to find list of equipment by manufacturer made before a certain year: ");
         System.out.println("Manufacturer of equipment: ");
         String manufacturer = scan.nextLine();
         System.out.println("Year to find equipment released before: ");
@@ -52,6 +60,8 @@ public class ExtraQueries {
     }
 
     private static void extraQuery2(Scanner scan) {
+        System.out.println(
+                "Query to find the watering equipment checked out by a member: ");
         System.out
                 .println("Enter member ID for their watering equipment info: ");
         int id = scan.nextInt();
@@ -68,6 +78,69 @@ public class ExtraQueries {
             while (rs.next()) {
                 System.out.println("Info: " + rs.getInt("Serial_Number")
                         + rs.getDate("Checkout_date"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    private static void extraQuery3(Scanner scan) {
+        System.out.println(
+                "Query to find employees who are also members of our service: ");
+
+        String sql = "SELECT    COUNT(MEMBER.email) AS Both_member_employee\n"
+                + "        FROM    MEMBER, EMPLOYEE\n"
+                + "        WHERE         EMPLOYEE.email = MEMBER.email ;";
+        try {
+            PreparedStatement stmt = DBConnection.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("Number of employees who are also members: "
+                        + rs.getInt("Both_member_employee"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    private static void extraQuery4(Scanner scan) {
+        System.out.println("Query to find average rating each equipment: ");
+
+        String sql = "SELECT    EQUIPMENT.serial_number AS equipment_num, AVG(rating) AS avg_rating\n"
+                + "    FROM      REVIEW, EQUIPMENT\n"
+                + "    WHERE     REVIEW.serial_number = EQUIPMENT.serial_number\n"
+                + "    GROUP BY EQUIPMENT.serial_number;";
+        try {
+            PreparedStatement stmt = DBConnection.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(
+                        "Equipment serial number: " + rs.getInt("equipment_num")
+                                + "\nrating: " + rs.getDouble("avg_rating"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    private static void extraQuery5(Scanner scan) {
+        System.out.println("Query to find the top three pieces of equipment: ");
+
+        String sql = "SELECT serial_number, COUNT(*) AS total\n"
+                + "FROM   WAREHOUSE_ORDER AS W, EQUIPMENT AS E\n"
+                + "WHERE  W.order_number = E.order_number\n"
+                + "    GROUP BY E.serial_number\n" + "    ORDER BY total\n"
+                + "    LIMIT 3;";
+        try {
+            PreparedStatement stmt = DBConnection.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(
+                        "Equipment serial number: " + rs.getInt("serial_num")
+                                + "\nrating: " + rs.getDouble("avg_rating"));
             }
         } catch (SQLException e) {
             System.out.println(e);
